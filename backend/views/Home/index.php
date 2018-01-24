6d5365ea8e120cf1101f39bb383b7e045fb0f180
Maciej Molenda <maciekm777@gmail.com>
2018-01-24 12:50:42 +0100

    Update index.php

--- backend/views/Home/index.php
+++ backend/views/Home/index.php
@@ -5,14 +5,20 @@
 </div> -->
 <!-- Główna treść strony -->
 <main>
-	<div class="container">
-		<div id="pani" class="text-left">
-		<div><a href="<?php echo ROOT_URL; ?>sklep/kategoria/9"><img src="<?php echo ROOT_URL; ?>assets/img/baba.png" alt="" width="355" height="440" /></a></div>
-		<div><a class="main-buttons" href="<?php echo ROOT_URL; ?>sklep/kategoria/9">Pani</a></div>
-		</div>
+	<div class="main_page">
+		<div class="container h-100">
+			<div class="row align-items-center h-100 pb-5">
+				<div class="col">			
+					<div id="pani" class="text-left fade-in-left">
+					<div><a href="<?php echo ROOT_URL; ?>sklep/kategoria/9"><img src="<?php echo ROOT_URL; ?>assets/img/baba.png" alt="" /></a></div>
+					<div><a class="main-buttons" href="<?php echo ROOT_URL; ?>sklep/kategoria/9">Pani</a></div>
+					</div>
 
-		<div id="pan" class="text-right"><div><a href="<?php echo ROOT_URL; ?>sklep/kategoria/1"><img src="<?php echo ROOT_URL; ?>assets/img/dziad.png" alt="" width="357" height="458" /></a></div>
-		<div><a class="main-buttons" href="<?php echo ROOT_URL; ?>sklep/kategoria/1">Pan</a></div>
+					<div id="pan" class="text-right fade-in-right"><div><a href="<?php echo ROOT_URL; ?>sklep/kategoria/1"><img src="<?php echo ROOT_URL; ?>assets/img/dziad.png" alt="" /></a></div>
+					<div><a class="main-buttons" href="<?php echo ROOT_URL; ?>sklep/kategoria/1">Pan</a></div>
+					</div>					
+				</div>				
+			</div>
 		</div>
 	</div>
 </main> <!-- koniec głównej treści strony -->
