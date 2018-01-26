98ba2183d918b382f44501e6759d59c78e474092
Maciej Molenda <maciekm777@gmail.com>
2018-01-26 16:57:35 +0100

    Update main.php

--- backend/views/main.php
+++ backend/views/main.php
@@ -9,6 +9,9 @@
 	<link rel="stylesheet" href="<?php echo ROOT_URL; ?>assets/css/bootstrap.min.css">
 	<link rel="stylesheet" href="<?php echo ROOT_URL; ?>assets/css/font-awesome.css">
 	<link rel="stylesheet" href="<?php echo ROOT_URL; ?>assets/css/styles.css">
+		<script src="<?php echo ROOT_URL; ?>assets/js/jquery.min.js"></script>
+		<script src="<?php echo ROOT_URL; ?>assets/js/popper.min.js"></script>
+		<script src="<?php echo ROOT_URL; ?>assets/js/bootstrap.min.js"></script>
 </head>
 <body>
 
@@ -28,12 +31,12 @@
 
 						<?php }else { ?>
 
-							<a href="<?php echo ROOT_URL; ?>users/login" class="pr-2"><i class="fa fa-user"></i> Zaloguj się</a>
+							<a href="<?php echo ROOT_URL; ?>users/login" class="pr-2"><i class="fa fa-user gold-color"></i> Zaloguj się</a>
 
 						<?php } ?>
 						</div>
 						<div class="col-sm-6 col-md-4 col-lg-3 dropdown-basket">
-							<a class="basket float-right pr-xl-4"  data-toggle="dropdown" href="./koszyk.html" title="koszyk">
+							<a class="basket float-right pr-xl-4" href="./koszyk.html" title="koszyk">
 								<span class="amount pr-4">0 zł</span>
 								<span class="count"></span>
 								<i class="fa fa-shopping-basket" aria-hidden="true"></i>
@@ -55,14 +58,14 @@
 						</div>
 					</div>
 					<div class="row col-md-12">
-						<nav class="navbar navbar-expand-sm navbar-light">
+						<nav class="navbar navbar-expand-sm navbar-light mx-auto mx-sm-0">
 							<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
 								<span class="navbar-toggler-icon"></span> MENU
 							</button>   
 							<div class="collapse navbar-collapse" id="collapsibleNavbar">
 								<ul class="nav text-center flex-column flex-sm-row align-items-start">
 									<li class="nav-item dropdown" >
-										<a href="<?php echo ROOT_URL; ?>sklep/kategoria/9" class="nav-link dropdown-toggle" data-toggle="dropdown">Pani</a>
+										<a href="<?php echo ROOT_URL; ?>sklep/kategoria/9" class="nav-link dropdown-toggle">Pani</a>
 										<ul class="my-dropdown-menu">
 											<li><a class="dropdown-item" href="#">Pozycja 1</a></li>
 												<ul>
@@ -76,7 +79,7 @@
 										</ul>
 									</li>
 									<li class="nav-item dropdown">
-										<a href="<?php echo ROOT_URL; ?>sklep/kategoria/1" class="nav-link dropdown-toggle"  data-toggle="dropdown">Pan</a>
+										<a href="<?php echo ROOT_URL; ?>sklep/kategoria/1" class="nav-link dropdown-toggle">Pan</a>
 										<ul class="my-dropdown-menu">
 											<li>
 												<a class="dropdown-item" href="#">Pozycja 1</a></li>
@@ -115,8 +118,6 @@
 			</div>
 		</footer> <!-- koniec stopki -->
 
-		<script src="<?php echo ROOT_URL; ?>assets/js/jquery.min.js"></script>
-		<script src="<?php echo ROOT_URL; ?>assets/js/popper.min.js"></script>
-		<script src="<?php echo ROOT_URL; ?>assets/js/bootstrap.min.js"></script>
+
 	</body>
 	</html>
