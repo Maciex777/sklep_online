3a8cad3846b11b5f88edfd46a073b69d955a2782
Maciej Molenda <maciekm777@gmail.com>
2018-01-26 16:59:35 +0100

    Add files via upload

--- backend/views/Sklep/produkt.php
+++ backend/views/Sklep/produkt.php
@@ -5,21 +5,24 @@
 	  <div class="row">
 
 		<div class="col-lg-3">
-      <div class="pt-5">
-        <?php if ($_SESSION['category'] === "Pan"){?>
-      <a href="<?php echo ROOT_URL; ?>sklep/kategoria/1"><img src="<?php echo ROOT_URL;?>assets/img/dziad.png" alt="" class="rotate-object" width="267" height="368"/></a>
-    <?php }else{?>
-      <a href="<?php echo ROOT_URL; ?>sklep/kategoria/9"><img src="<?php echo ROOT_URL;?>assets/img/baba.png" alt="" class="rotate-object" width="267" height="368"/></a>
-    <?php }?>
-      </div>
-      <h1 class="my-4"><?php echo $_SESSION['category'];?></h1>
+			<a href="<?php echo ROOT_URL; ?>sklep/produkty/<?php echo $viewmodel['product_category']; ?>" class="btn my-button standard-buttons btn-lg px-5 py-2 mt-5"><i class="fa fa-arrow-left pr-2" aria-hidden="true"></i> Wróć</a>
+		  <div class="pt-5">
+			<?php if ($_SESSION['category'] === "Pan"){?>
+			  <a href="<?php echo ROOT_URL; ?>sklep/kategoria/1"><img src="<?php echo ROOT_URL;?>assets/img/dziad.png" alt="" class="rotate-object img-fluid" width="267"/></a>
+			<?php }else{?>
+			  <a href="<?php echo ROOT_URL; ?>sklep/kategoria/9"><img src="<?php echo ROOT_URL;?>assets/img/baba.png" alt="" class="rotate-object img-fluid" width="267"/></a>
+			<?php }?>
+		  </div>
+		  <div class="text-center" style="border:1px solid #000">
+				<h1 class="my-4"><strong><?php echo $_SESSION['category'];?></strong></h1>
+		  </div>
 		</div>
 		<!-- /.col-lg-3 -->
 <?php  //foreach ($viewmodel as $item) {
 
   ?>
+		
 		<div class="col-lg-9">
-
 		  <div class="card mt-4">
 			<img class="card-img-top img-fluid" src="<?php echo ROOT_URL; echo $cat['category_photo']; ?>" alt="">
 			<div class="card-body">
@@ -38,10 +41,11 @@
 			<div class="card-body">
 			  <p><?php echo $viewmodel['product_description']; ?></p>
 			  <hr>
-				<form action="<?php echo ROOT_URL;?>sklep/produkt/<?php echo $viewmodel['product_id'];?>" method="post">
+				<button type="button" class="btn my-button standard-buttons btn-lg px-5 py-2" name="dodaj" id = "add_to_cart" >Dodaj do koszyka</button>
+				<!-- <form action="<?php //echo ROOT_URL;?>sklep/produkt/<?php// echo $viewmodel['product_id'];?>" method="post">
 
 				<input type="submit" name="cart_action" value="Dodaj do koszyka">
-				</form>
+				</form> -->
 				<?php //if (isset($_SESSION['cart'])){
 				//echo "<pre>";
 				//print_r($_SESSION['cart']);
@@ -58,3 +62,21 @@
 
 	</div>
 	</main> <!-- koniec głównej treści strony -->
+<script type="text/javascript">
+
+$(document).ready(function(){
+	$("#add_to_cart").click(function(){
+
+		$.ajax({
+        url: '<?php echo ROOT_URL;?>expansion/Addtocart.php',
+        type: 'POST',
+
+        success:function(response){
+           alert("Dodano do koszyka");
+        }
+   });
+	});
+});
+
+
+</script>
