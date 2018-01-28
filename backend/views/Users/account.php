c322f4fc9573b0d85957c979d3577f0cb61f6bb7
Maciej Molenda <maciekm777@gmail.com>
2018-01-28 17:26:29 +0100

    Add files via upload

---
+++ backend/views/Users/account.php
@@ -0,0 +1,103 @@
+<main>
+    <div class="container">
+        <div class="col-md-12 mt-5">							
+                        <h1>Moje konto</h1>
+						
+		                <h3 class="mt-4">Dane osobiste</h3>
+                        <form>
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <label for="firstname">Imię</label>
+                                        <h3></h3>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <label for="lastname">Nazwisko</label>
+                                        <h3></h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /.row -->
+
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <label for="city">Miasto</label>
+                                        <h3></h3>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6 col-md-3">
+                                    <div class="form-group">
+                                        <label for="street">Ulica</label>
+                                        <h3></h3>
+                                    </div>
+                                </div>
+                            
+                                <div class="col-sm-6 col-md-3">
+                                    <div class="form-group">
+                                        <label for="kod">Kod pocztowy</label>
+                                        <h3></h3>
+                                    </div>
+                                </div>
+                              </div>
+                            <!-- /.row -->
+							
+							<div class="row">
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <label for="phone">Telefon</label>
+                                        <h3></h3>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <label for="email">E-mail</label>
+                                        <h3></h3>
+                                    </div>
+                                </div>
+                                <div class="col-sm-12 text-center">
+
+                                </div>
+                            </div>
+                        </form>
+
+                        <hr>
+						 <h3 class="mt-4">Zmień hasło</h3>
+
+                        <form>
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <label for="password_old">Stare hasło</label>
+                                        <input type="password" class="form-control" id="password_old">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <label for="password_1">Nowe hasło</label>
+                                        <input type="password" class="form-control" id="password_1">
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <label for="password_2">Powtórz nowe hasło</label>
+                                        <input type="password" class="form-control" id="password_2">
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /.row -->
+
+                            <div class="col-sm-12 text-center">		
+							<!-- Button -->
+								<a href="" class="btn my-button standard-buttons btn-lg mt-3">Zapisz nowe hasło</a>
+
+                            </div>
+                        </form>
+                </div>
+
+    </div>
+	</main> <!-- koniec głównej treści strony -->
