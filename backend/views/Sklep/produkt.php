5f97bb58db951f6213977aa862b73f1362e281c6
Maciej Molenda <maciekm777@gmail.com>
2018-01-31 19:11:30 +0100

    Add files via upload

--- backend/views/Sklep/produkt.php
+++ backend/views/Sklep/produkt.php
@@ -16,10 +16,8 @@
 							  <a href="<?php echo ROOT_URL; ?>sklep/kategoria/9"><img src="<?php echo ROOT_URL;?>assets/img/baba.png" alt="" class="rotate-object img-fluid" width="267"/></a>
 							<?php }?>
 						  </div>
-
-						  <div class="text-center" style="border-top:1px solid silver">
+						  <div class="text-center category-title">
 								<h2 class="my-4"><strong><a href="<?php echo ROOT_URL;?>sklep/kategoria/<?php if ($_SESSION['category'] === "Pan") {echo "1";}else{echo "9";} ?> "><?php echo $_SESSION['category'];?></a> </strong></h2>
-
 								<hr>
 								<h3> <a href="<?php echo ROOT_URL;?>sklep/produkty/<?php echo $_SESSION['category_id']; ?>"> <?php echo $_SESSION['current_category']; ?></a></h3>
 						  </div>
@@ -35,8 +33,9 @@
 					<div class="product_details_title">
 						<h2><?php echo $viewmodel['product_name']; ?></h2>
 						<p><?php echo $viewmodel['product_description']; ?></p>
+					<button type="button" class="btn my-button standard-buttons btn-lg px-5 py-2 mt-2" name="dodaj" id = "add_to_cart" >Dodaj do koszyka</button>
 					</div>
-					<div class="free_delivery d-flex flex-row align-items-center justify-content-center">
+					<div class="free_delivery d-flex flex-row align-items-center justify-content-center mt-4">
 						<span> darmowa dostawa</span>
 					</div>
 					<span>Cena:</span>
@@ -53,15 +52,11 @@
 							<span class="plus" id="plus"><i class="fa fa-plus"   aria-hidden="true"></i></span>
 						</div>
 					</div>
-					<div class="" id="count_in_cart_div">
+					<div class="pt-4" id="count_in_cart_div">
 					<?php if (isset($_SESSION['cart']) && isset($_SESSION['cart'][$viewmodel['product_id']]) && !$_SESSION['cart'][$viewmodel['product_id']] == null){ ?>
-
-					<!-- <div class="" id="count_in_cart_div"> -->
-						W koszyku: <span id="count_in_cart"> <?php echo $_SESSION['cart'][$viewmodel['product_id']]; ?></span>
-					<!-- </div> --><?php } ?>
-					</div>
-
-					<button type="button" class="btn my-button standard-buttons btn-lg px-5 py-2 mt-4" name="dodaj" id = "add_to_cart" >Dodaj do koszyka</button>
+						W koszyku: &nbsp;<strong id="count_in_cart"> <?php echo $_SESSION['cart'][$viewmodel['product_id']]; ?></strong>
+					</div><?php } ?>
+					<a type="button" href="<?php echo ROOT_URL; ?>sklep/koszyk" class="btn my-button standard-buttons btn-lg px-5 py-2 mt-4" >Przejdź do koszyka</a>
 				</div>
 			</div>
 		</div>
@@ -91,33 +86,33 @@ $(document).ready(function(){
 				data: {product_cost , product_id , product_count},
         success:function(response){
 
-
  	        }
 
    });
+   
+		$("#cart_value").text(parseInt($("#cart_value").text()) + (product_cost*product_count));
+		 if ($("#count_in_cart").length){
+			$("#count_in_cart").text(parseInt($("#count_in_cart").text()) + product_count);
+		} else {
+			$("#count_in_cart_div").append("W koszyku: <span id=\"count_in_cart\"> </span>");
+			$("#count_in_cart").text(product_count);
+		}
+	  $("#mini-cart").text(" ");
+		$.ajax({
+	
+		url : "<?php echo ROOT_URL;?>views/koszyk.php",
+		type: "post",
+		dataType: 'json',
+		success : function(response) {
+		//alert(Object.keys(response).length)
+		$.each(response,function(i, value){
+			var cart_item;
+			 cart_item = "<div class=\"row item\"><div class=\"col-5\"><img class=\"img-fluid d-flex flex-column\" alt=\""+value['product_name']+"\" src=\"<?php echo ROOT_URL; ?>"+value['product_image']+"\" /></div><div class=\"col-7\"><p class=\"h5 text-right\">"+value['product_name']+"</p><p class=\"text-right\"><b class=\"count-item\"></b><b>"+value['product_cost']+"zł</b></p></div></div>";
+			 $("#mini-cart").append(cart_item);
+		});
+		}
+	  });
 
-	 $("#cart_value").text(parseInt($("#cart_value").text()) + (product_cost*product_count));
-	 if ($("#count_in_cart").length){
-		$("#count_in_cart").text(parseInt($("#count_in_cart").text()) + product_count);
-	} else {
-		$("#count_in_cart_div").append("W koszyku: <span id=\"count_in_cart\"> </span>");
-		$("#count_in_cart").text(product_count);
-	}
-	$("#mini-cart").text(" ");
-	$.ajax({
-
-	url : "<?php echo ROOT_URL;?>views/koszyk.php",
-	type: "post",
-	dataType: 'json',
-	success : function(response) {
-	//alert(Object.keys(response).length)
-	$.each(response,function(i, value){
-		var cart_item;
-		 cart_item = "<div class=\"row item\"><div class=\"col-5\"><img class=\"img-fluid d-flex flex-column\" alt=\""+value['product_name']+"\" src=\"<?php echo ROOT_URL; ?>"+value['product_image']+"\" /></div><div class=\"col-7\"><p class=\"h5 text-right\">"+value['product_name']+"</p><p class=\"text-right\"><b class=\"count-item\"></b><b>"+value['product_cost']+"zł</b></p></div></div>";
-		 $("#mini-cart").append(cart_item);
-	});
-	}
-	});
 	});
 });
 </script>
