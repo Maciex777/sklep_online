94c115a903b131d1fb19bcc320ed595bfb70bd62
Maciej Molenda <maciekm777@gmail.com>
2018-02-01 11:57:05 +0100

    Add files via upload

--- backend/views/main.php
+++ backend/views/main.php
@@ -231,38 +231,63 @@
 	</header> <!-- koniec nagłówka -->
 
 		<?php require($view); ?>
-
-
-	<!-- Stopka -->
+		
+<!-- Stopka -->		
 <footer>
-	<div class="py-5 bg-dark text-white">
-    <div class="container">
-      <div class="row">
-        <div class="col-12 text-center align-self-center">
-          <p class="halka"><strong>Halka - Sklep z Odzieżą dla seniora</strong></p>
-					<p class="adress">Adressssss</p>
-					<p class="tel">tel. (+48) 456-7890 </p>	
-					<div class="pt-5 my-3 row">
-            <div class="col-4">
-              <a href="https://www.facebook.com" target="_blank"><i class="fa fa-3x fa-facebook"></i></a>
-            </div>
-            <div class="col-4">
-              <a href="https://twitter.com" target="_blank"><i class="fa fa-3x fa-twitter"></i></a>
-            </div>
-            <div class="col-4">
-              <a href="https://www.instagram.com" target="_blank"><i class="fa fa-3x fa-instagram"></i></a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-12 pb-3 pt-5 clearfix text-center align-self-center">
-          <p> <strong>&copy; Halka 2018</strong></p>
-        </div>
+		<div class="container text-center text-md-left pt-5">
+			<div class="row pt-4">
+			  <div class="col-md-4 col-sm-12">
+				<!--Column1-->
+				<div class="footer-pad">
+				  <h4>Kontakt</h4>
+				  <address>
+						<ul class="list-unstyled">
+							<li>
+								+48 22 123 12 12<br>
+							</li>
+							<li>
+								halkasklep@wp.pl
+							</li>
+							<li>
+								ul. Przykładowa 1/4 <br>
+								12-123 Warszawa
+							</li>
+						</ul>
+					</address>
+				</div>
+			  </div>
+			  <div class="col-md-4 col-sm-12">
+				<!--Column1-->
+				<div class="footer-pad">
+				  <h4>Katalog produktów</h4>
+				  <ul class="list-unstyled">
+					<li><a href="<?php echo ROOT_URL; ?>sklep/kategoria/9">Pani</a></li>
+					<li><a href="<?php echo ROOT_URL; ?>sklep/kategoria/1">Pan</a></li>
+				  </ul>
+				</div>
+			  </div>
+			  <div class="col-md-4 col-sm-12">
+				<!--Column1-->
+				<div class="footer-pad">
+				  <h4>Informacje o sklepie</h4>
+				  <ul class="list-unstyled">
+					<li><a href="<?php echo ROOT_URL; ?>home/about">O nas</a></li>
+					<li><a href="<?php echo ROOT_URL; ?>home/regulamin">Regulamin</a></li>
+				  </ul>
+				</div>
+			  </div>
 			</div>
+			<hr>
+			<div class="row">
+				<div class="col-12 text-center pt-5">
+				  <h5 class="halka"><strong>Halka - Sklep z Odzieżą dla seniora</strong></h5>
+				</div>
+				<div class="col-12 py-4 text-center">
+					<p> <strong>&copy; Halka 2018</strong></p>
+				</div>
+			</div>			
 	</div>
-</footer> 
-<!-- koniec stopki -->
+</footer>
 
 <!-- Strzałka przekierowująca do góry -->
 <button id="myBtn" title="Go to top"></button>
