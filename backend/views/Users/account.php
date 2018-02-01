3dbd7e23b5c3d22f855664fbabf2b99639dae0c3
Maciej Molenda <maciekm777@gmail.com>
2018-02-01 19:49:21 +0100

    Add files via upload

--- backend/views/Users/account.php
+++ backend/views/Users/account.php
@@ -1,24 +1,25 @@
 <main>
 
     <div class="container">
-        <div class="col-md-12 mt-5">
-                        <h1>Moje konto</h1>
+        <div class="col-md-12 mt-5 pt-4">
+                        <h1 class="heading-font text-center">Moje konto</h1>
 <h2>  <?php if (isset($_SESSION['response'])){
   echo $_SESSION['response'];
 }?></h2>
 
 		                <h3 class="mt-4">Dane osobiste</h3>
+						<hr>
                         <form>
                             <div class="row">
                                 <div class="col-sm-6">
                                     <div class="form-group">
-                                        <label for="firstname">Imię</label>
+                                        <label for="firstname" class="text-muted">Imię</label>
                                         <h3><?php echo $_SESSION['user_data']['name']; ?></h3>
                                     </div>
                                 </div>
                                 <div class="col-sm-6">
                                     <div class="form-group">
-                                        <label for="lastname">Nazwisko</label>
+                                        <label for="lastname" class="text-muted">Nazwisko</label>
                                         <h3><?php echo $_SESSION['user_data']['surname']; ?></h3>
                                     </div>
                                 </div>
@@ -27,20 +28,20 @@
                             <div class="row">
                                 <div class="col-sm-6">
                                     <div class="form-group">
-                                        <label for="city">Ulica</label>
+                                        <label for="city" class="text-muted">Ulica</label>
                                         <h3><?php echo $viewmodel['street']; ?></h3>
                                     </div>
                                 </div>
                                 <div class="col-sm-6 col-md-3">
                                     <div class="form-group">
-                                        <label for="street">Dom</label>
+                                        <label for="street" class="text-muted">Dom</label>
                                         <h3><?php echo $viewmodel['building']; ?></h3>
                                     </div>
                                 </div>
 
                                 <div class="col-sm-6 col-md-3">
                                     <div class="form-group">
-                                        <label for="kod">Mieszkanie</label>
+                                        <label for="kod" class="text-muted">Mieszkanie</label>
                                         <h3><?php echo $viewmodel['appartment'];?></h3>
                                     </div>
                                 </div>
@@ -49,20 +50,20 @@
                             <div class="row">
                                 <div class="col-sm-6">
                                     <div class="form-group">
-                                        <label for="city">Miasto</label>
+                                        <label for="city" class="text-muted">Miasto</label>
                                         <h3><?php echo $viewmodel['city']; ?></h3>
                                     </div>
                                 </div>
                                 <div class="col-sm-6 col-md-3">
                                     <div class="form-group">
-                                        <label for="street">Ulica</label>
+                                        <label for="street" class="text-muted">Ulica</label>
                                         <h3><?php echo $viewmodel['street']; ?></h3>
                                     </div>
                                 </div>
 
                                 <div class="col-sm-6 col-md-3">
                                     <div class="form-group">
-                                        <label for="kod">Kod pocztowy</label>
+                                        <label for="kod" class="text-muted">Kod pocztowy</label>
                                         <h3><?php echo $viewmodel['post_code'];?></h3>
                                     </div>
                                 </div>
@@ -72,13 +73,13 @@
 							<div class="row">
                                 <div class="col-sm-6">
                                     <div class="form-group">
-                                        <label for="phone">Telefon</label>
+                                        <label for="phone" class="text-muted">Telefon</label>
                                         <h3><?php echo $_SESSION['user_data']['phone_number']; ?></h3>
                                     </div>
                                 </div>
                                 <div class="col-sm-6">
                                     <div class="form-group">
-                                        <label for="email">E-mail</label>
+                                        <label for="email" class="text-muted">E-mail</label>
                                         <h3><?php echo $_SESSION['user_data']['email']; ?></h3>
                                     </div>
                                 </div>
@@ -89,11 +90,11 @@
                         </form>
 
                         <hr>
-						 <h3 class="mt-4">Zmień hasło</h3>
+						 <h3 class="my-4">Zmień hasło</h3>
 
-                        <form action="<?php echo ROOT_URL;?>users/account" method="post">
+                        <form action="<?php echo ROOT_URL;?>users/account" method="post" class="mx-auto">
                             <div class="row">
-                                <div class="col-sm-6">
+                                <div class="col-lg-6">
                                     <div class="form-group">
                                         <label for="password_old">Stare hasło</label>
                                         <input type="password" class="form-control" id="password_old" name="old_password">
@@ -101,14 +102,16 @@
                                 </div>
                             </div>
                             <div class="row">
-                                <div class="col-sm-6">
+                                <div class="col-lg-6">
                                     <div class="form-group">
                                         <label for="password_1">Nowe hasło</label>
                                         <input type="password" class="form-control" id="password_1"
                                         name="new_password_1">
                                     </div>
                                 </div>
-                                <div class="col-sm-6">
+							</div>
+                            <div class="row">
+                                <div class="col-lg-6">
                                     <div class="form-group">
                                         <label for="password_2">Powtórz nowe hasło</label>
                                         <input type="password" class="form-control" id="password_2" name="new_password_2">
@@ -117,10 +120,9 @@
                             </div>
                             <!-- /.row -->
 
-                            <div class="col-sm-12 text-center">
+                            <div class="col-lg-6 text-center text-md-left">
 							<!-- Button -->
-								<!-- <a href="" class="btn my-button standard-buttons btn-lg mt-3">Zapisz nowe hasło</a> -->
-                <button type="submit" name="submit" value="password_change" class="btn my-button standard-buttons btn-lg mt-3">Zmień hasło</button>
+								<button type="submit" name="submit" value="password_change" class="btn my-button standard-buttons btn-lg mt-3">Zmień hasło</button>
                             </div>
                         </form>
                 </div>
--- backend/views/Users/register.php
+++ backend/views/Users/register.php
@@ -4,8 +4,8 @@
   <div class="row">
     <div class="col-md-8 mx-auto mt-4">
     <div class="block text-center">
-      <h2 class="heading-font text-center mt-5">Utwórz konto</h2>
-      <p class="text-center py-4"> Dla Twojej wygody uprościliśmy proces rejestracji w sklepie Halka. <br/>Podaj swoje dane a nasza obsługa klienta skontaktuje się z Tobą telefonicznie w przeciągu 24 godzin w celu założenia konta na Halka.com.pl</p>
+      <h1 class="heading-font text-center mt-5">Utwórz konto</h1>
+      <p class="text-center py-4 register-text"> Dla Twojej wygody uprościliśmy proces rejestracji w sklepie Halka. <br/>Podaj swoje dane a nasza obsługa klienta skontaktuje się z Tobą telefonicznie w przeciągu 24 godzin w celu założenia konta na Halka.com.pl</p>
       <form class="text-left clearfix mt-30" action="<?php echo ROOT_URL; ?>mailer/mail.php" method="post">
       <div class="row">
 	  <div class="form-group col-12 col-md-6">
@@ -21,19 +21,12 @@
         <input type="tel" name="phone" class="form-control"  placeholder="Numer telefonu" required="required">
       </div>
       </div>
-      <button type="submit" id="submit" class="btn my-button standard-buttons btn-lg px-5 py-2 mt-2" value="register">Wyślij zgłoszenie</button>
+	   <button type="submit" class="btn my-button standard-buttons btn-lg px-5 py-2 mt-2 submit" value="register">Wyślij zgłoszenie</button>
       </form>
-      <p class="mt-4 text-left">Masz już konto ?<a href="<?php echo ROOT_URL; ?>users/login"> Zaloguj się</a></p>
+      <p class="mt-4 text-left">Masz już konto ?<a href="<?php echo ROOT_URL; ?>users/login"><strong> Zaloguj się</strong></a></p>
 
     </div>
     </div>
   </div>
   </div>
 </main> <!-- koniec głównej treści strony -->
-<script type="text/javascript">
-
-
-$("#submit").click(function(){
-  alert("Dziękujemy za wysłanie prośby o kontakt. Proszę oczekiwać kontaktu telefonicznego.")
-})
-</script>
