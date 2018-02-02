5b94a8f0e0fc3a4c5be0253cb868288387b99ebc
Maciej Molenda <maciekm777@gmail.com>
2018-02-02 06:25:56 +0100

    Add files via upload

--- backend/views/main.php
+++ backend/views/main.php
@@ -152,6 +152,27 @@
 <!-- Strzałka przekierowująca do góry -->
 <button id="myBtn" title="Go to top"></button>
 
+<script>
+//asynchroniczne dodawanie do koszyka
+$(document).ready(function(){
+
+$.ajax({
+
+url : "<?php echo ROOT_URL;?>views/koszyk.php",
+type: "post",
+dataType: 'json',
+success : function(response) {
+//alert(Object.keys(response).length)
+$.each(response,function(i, value){
+	var cart_item;
+	 cart_item = "<div class=\"row item\"><div class=\"col-5\"><img class=\"img-fluid d-flex flex-column\" alt=\""+value['product_name']+"\" src=\"<?php echo ROOT_URL; ?>"+value['product_image']+"\" /></div><div class=\"col-7\"><p class=\"h5 text-right\">"+value['product_name']+"</p><p class=\"text-right\"><b class=\"count-item\"></b><b>"+value['product_cost']+"zł</b></p></div></div>";
+	 $("#mini-cart").append(cart_item);
+});
+}
+});
+});
+</script>
+
 <script src="<?php echo ROOT_URL; ?>assets/js/popper.min.js"></script>
 <script src="<?php echo ROOT_URL; ?>assets/js/bootstrap.min.js"></script>
 
